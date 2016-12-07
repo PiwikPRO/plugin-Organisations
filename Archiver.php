07f3a37e14d97ee7e08f898e596d799f47632a34
Marcin Wierzbiński <marcin119a@gmail.com>
2016-12-07 17:49:03 +0100

    License upgrade

--- Archiver.php
+++ Archiver.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ *  Piwik - free/libre analytics platform
+
+ *  Piwik is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  Piwik is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+
+ *  @link http://piwik.pro
+ *  @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
 namespace Piwik\Plugins\Organisations;
 
 use Piwik\Metrics;
